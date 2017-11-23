c4be27dc33007a6c3462ab77d37188348f2b7084
Ruedi <ruedi.luethi@mitlinks.ch>
2017-11-23 12:43:08 +0100

    multiples_wachstum added, plus some notes

--- Code/logistisches_wachstum_mit_schrittweite.m
+++ Code/logistisches_wachstum_mit_schrittweite.m
@@ -1,20 +1,23 @@
 clear;
 close all;
 
-%duration = 600;
+%delta = .1;
+%duration = 600/delta;
 %r = 0.06;
 %K = 150000;
 %pop_0 = 1;
 
-%duration = 12;
-%r = 3.8; % chaos...?
-%K = 100;
-%pop_0 = .01;
-
-duration = 20;
-r = 1;
+delta = .1;
+duration = 12/delta;
+r = 2; % chaos...?
 K = 100;
 pop_0 = 1;
+
+%delta = 0.1;
+%duration = 20/delta;
+%r = 1;
+%K = 100;
+%pop_0 = 1;
 
 E = r/2; % max zulässiger Ertrag, damit weiterhin stabil
 
@@ -30,17 +33,17 @@ ypop_change = [0];
 ypop_yield = [0];
 
 for i = [t(1):t(end-1)]
-    pop_change(end+1) = r*(1-(pop(end)/K))*pop(end);
-    pop_yield(end+1) = E*pop(end);
+    pop_change(end+1) = r*(1-(pop(end)/K))*pop(end)*delta;
+    pop_yield(end+1) = E*pop(end)*delta;
     pop(end+1) = pop(end) + pop_change(end); % xn + Zuwachs
     
-    ypop_change(end+1) = r*(1-ypop(end)/K)*ypop(end);
-    ypop_yield(end+1) = E*ypop(end);
+    ypop_change(end+1) = r*(1-ypop(end)/K)*ypop(end)*delta;
+    ypop_yield(end+1) = E*ypop(end)*delta;
     ypop(end+1) = ypop(end) + ypop_change(end) - ypop_yield(end); % xn + Zuwachs - Ertrag
 end
 
 subplot(2,2,1);
-plot(t,pop,t,pop_change);
+plot(t,pop,t,pop_change/delta);
 
 subplot(2,2,2);
 plot(pop(1:end-1),pop(2:end)-pop(1:end-1),pop(1:end-1),pop_yield(1:end-1));
---
+++ Code/multiples_logistisches_wachstum.m
@@ -0,0 +1,38 @@
+clear;
+close all;
+
+%duration = 600;
+%r = 0.06;
+%K = 150000;
+
+duration = 400;
+r = 1;
+K = 100;
+
+E = r/2; % max zulässiger Ertrag, damit weiterhin stabil
+
+t = 1:duration;
+
+popA = [1];
+popB = [1];
+
+for i = [t(1):t(end-1)]
+    
+    K_tot = (popA(end)+popB(end))/K;
+    
+    rA = 1;
+    popA_new_born = rA*(1-K_tot)*popA(end);
+    popA_killed_by_B = popB(end)*popA(end)*0.01;
+    popA(end+1) = popA(end) + popA_new_born - popA_killed_by_B;
+    
+    rB = popA(end)/(K-popB(end));
+    popB_new_born = rB*(1-K_tot)*popB(end);
+    popB_killed = popB(end)*0.02;
+    popB(end+1) = popB(end) + popB_new_born - popB_killed;
+    
+end
+
+%subplot(2,2,1);
+plot(t,popA,t,popB);
+axis([0 duration 0 K]);
+legend('Population A', 'Population B');
