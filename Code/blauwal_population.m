4747dade49f31bb0359f364bf7b8360549b06bfa
Ruedi <ruedi.luethi@mitlinks.ch>
2017-11-25 11:36:31 +0100

    some scaling problems (with delta_t) in the blauwal diagrams corrected

--- Code/blauwal_population.m
+++ Code/blauwal_population.m
@@ -3,15 +3,17 @@ close all;
 
 delta_t = .1;
 
+% Blauwal data
 duration = 600;
 r = 0.06;
 K = 150000;
-pop_0 = 1;
 
+% Test data
 %duration = 20;
 %r = 1;
 %K = 100;
-%pop_0 = 1;
+
+pop_0 = 1;
 
 E = r/2; % max zulässiger Ertrag, damit weiterhin stabil
 
@@ -27,17 +29,20 @@ ypop_change = [0];
 ypop_yield = [0];
 
 for i = [t(1):t(end-1)]
-    pop_change(end+1) = r*(1-(pop(end)/K))*pop(end) *delta_t;
-    pop_yield(end+1) = E*pop(end) *delta_t;
-    pop(end+1) = pop(end) + pop_change(end); % xn + Zuwachs
+    pop_change(end+1) = r*(1-(pop(end)/K))*pop(end);
+    pop_yield(end+1) = E*pop(end);
+    pop(end+1) = pop(end) + pop_change(end)*delta_t; % xn + Zuwachs
     
-    ypop_change(end+1) = r*(1-ypop(end)/K)*ypop(end) *delta_t;
-    ypop_yield(end+1) = E*ypop(end) *delta_t;
-    ypop(end+1) = ypop(end) + ypop_change(end) - ypop_yield(end); % xn + Zuwachs - Ertrag
+    ypop_change(end+1) = r*(1-ypop(end)/K)*ypop(end);
+    ypop_yield(end+1) = E*ypop(end);
+    ypop(end+1) = ypop(end) + (ypop_change(end) - ypop_yield(end))*delta_t; % xn + Zuwachs - Ertrag
 end
 
 subplot(2,2,1);
-plot(t*delta_t,pop,t*delta_t,pop_change/delta_t);
+hold on;
+plot(t*delta_t,pop);
+plot(t*delta_t,pop_change);
+hold off;
 axis([ 0 duration*delta_t 0 K ]);
 title('Entwicklung der Population ohne äussere Einflüsse');
 legend('Population','Zuwachs');
@@ -51,7 +56,7 @@ max_yield = 0;
 delta_pop_yield = [];
 for i = [1:length(pop)-1]
     a = pop(i+1) - pop(i);
-    b = pop_yield(i);
+    b = pop_yield(i)*delta_t;
     delta_pop_yield(end+1) = abs(a-b);
     if (pop(i) > K*0.1 && delta_pop_yield(end) < min_distance)
         min_distance = delta_pop_yield(end);
@@ -61,7 +66,12 @@ for i = [1:length(pop)-1]
 end
 
 subplot(2,2,2);
-plot(pop(1:end-1),pop(2:end)-pop(1:end-1),pop(1:end-1),pop_yield(1:end-1),pop(1:end-1),delta_pop_yield,max_pop,max_yield,'ok');
+hold on;
+plot(pop(1:end-1),(pop(2:end)-pop(1:end-1))/delta_t); % difference xn - xn+1
+plot(pop(1:end-1),pop_yield(1:end-1));
+plot(pop(1:end-1),delta_pop_yield/delta_t);
+plot(max_pop,max_yield,'ok');
+hold off;
 title('Möglicher Ertrag aus der Population');
 legend('Zuwachs','Ertrag','Differenz Ertrag Zuwachs',['Max Ertrag'] );
 legend('Location','northwest');
@@ -78,7 +88,12 @@ xlabel('Zeit');
 ylabel('Anzahl Tiere');
 
 subplot(2,2,4);
-plot(t*delta_t,ypop_change,t*delta_t,ypop_yield,t*delta_t,ypop_change-ypop_yield,[1,duration],[max_yield,max_yield],'k');
+hold on;
+plot(t*delta_t,ypop_change);
+plot(t*delta_t,ypop_yield);
+plot(t*delta_t,ypop_change-ypop_yield);
+plot([1,duration],[max_yield,max_yield],'k');
+hold off;
 axis([ 0 duration*delta_t 0 K ]);
 axis 'auto y';
 title('Zuwachs und Ertrag im Vergleich');
