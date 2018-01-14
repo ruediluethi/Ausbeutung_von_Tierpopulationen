f73983986bcea2ed859b8234270565ec2fa0a49e
Ruedi <ruedi.luethi@mitlinks.ch>
2018-01-14 19:29:18 +0100

    final text added

--- Code/agent_model_plot.m
+++ Code/agent_model_plot.m
@@ -36,3 +36,26 @@ fig = gcf;
 fig.PaperUnits = 'centimeters';
 fig.PaperPosition = [0 0 7 7];
 print(['../Dokumentation/Diagramme/agent_model_oscillation.png'],'-dpng','-r300');
+
+% damped oscillation
+data = importdata('data/export_damped.txt');
+data = data';
+
+t = [1:length(data)];
+delta_t = 0.3;
+
+oscillation_center = 50000;
+view_delta = 3000;
+plot(t*delta_t,data(1,:),'k',t*delta_t,data(2,:),'k--');
+axis([0 3000 oscillation_center-view_delta oscillation_center+view_delta]); 
+
+legend('Räuber','Beute');
+legend('boxoff');
+legend('Location','northoutside');
+xlabel('Zeit');
+ylabel('Anzahl Tiere');
+
+fig = gcf;
+fig.PaperUnits = 'centimeters';
+fig.PaperPosition = [0 0 7 7];
+print(['../Dokumentation/Diagramme/agent_model_damped.png'],'-dpng','-r300');
