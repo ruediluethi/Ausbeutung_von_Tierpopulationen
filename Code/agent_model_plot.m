fa54de4172aedbddd96ac272b302480ee3905c70
Ruedi <ruedi.luethi@mitlinks.ch>
2018-01-14 12:07:18 +0100

    text for agent based modelling added

--- Code/agent_model_plot.m
+++ Code/agent_model_plot.m
@@ -1,7 +1,7 @@
 clear;
 close all;
 
-data = importdata('data/export_final.txt');
+data = importdata('data/export.txt');
 data = data';
 
 t = [1:length(data)];
@@ -9,7 +9,7 @@ delta_t = 0.3;
 
 % begin of the simulation
 plot(t*delta_t,data(1,:),'k',t*delta_t,data(2,:),'k--');
-axis([0 500 0 max(data(1,:))+min(data(2,:))]); 
+axis([0 200 0 max(data(1,:))+min(data(2,:))]); 
 
 legend('Räuber','Beute');
 legend('boxoff');
@@ -19,12 +19,12 @@ ylabel('Anzahl Tiere');
 
 fig = gcf;
 fig.PaperUnits = 'centimeters';
-fig.PaperPosition = [0 0 7 10];
+fig.PaperPosition = [0 0 7 7];
 print(['../Dokumentation/Diagramme/agent_model_start.png'],'-dpng','-r300');
 
 % oscillating behavior
 plot(t*delta_t,data(1,:),'k',t*delta_t,data(2,:),'k--');
-axis([1900 2200 0 max(data(1,:))+min(data(2,:))]); 
+axis([998 1174 0 max(data(1,:))+min(data(2,:))]); 
 
 legend('Räuber','Beute');
 legend('boxoff');
@@ -34,5 +34,5 @@ ylabel('Anzahl Tiere');
 
 fig = gcf;
 fig.PaperUnits = 'centimeters';
-fig.PaperPosition = [0 0 7 10];
+fig.PaperPosition = [0 0 7 7];
 print(['../Dokumentation/Diagramme/agent_model_oscillation.png'],'-dpng','-r300');
